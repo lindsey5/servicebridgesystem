870dc44d6341f717346c8c8a50457630c00e9c92
lindsey5 <lindseysamson5@gmail.com>
2024-10-17 22:27:31 +0800

    initial commit

--- frontend/src/Pages/ProviderPage/Availability.jsx
+++ frontend/src/Pages/ProviderPage/Availability.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect} from 'react';
-import './availability.css';
+import './Availability.css';
 
 const Availability = () => {
     const [date, setDate] = useState(new Date());
