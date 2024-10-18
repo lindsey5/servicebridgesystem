0ec830c81b39e74083f5be8e574dfb8e1a964050
lindsey5 <lindseysamson5@gmail.com>
2024-10-18 19:07:14 +0800

    initial commit

--- frontend/src/Pages/HomePage/header.jsx
+++ frontend/src/Pages/HomePage/header.jsx
@@ -1,18 +1,26 @@
 import './header.css';
+import { useState } from 'react';
 
 const Header = ({ showLoginModal, showSignupModal }) => {
+    const [showNav, setShowNav] = useState(false);
     return (
         <header className='home-header'>
             <h1><a href="#home" className='title'>Hustle</a></h1>
-            <button className="nav-button">
-                <img className="nav-icon" src="/icons/hamburger-menu.svg" alt="Menu Icon" />
+            <button className="nav-button" onClick={() => setShowNav(!showNav)}>
+                <img className="nav-icon" src="/icons/menu.png" alt="Menu Icon" />
             </button>
             <ul>
                 <li><a href="#home">Home</a></li>
                 <li><a href="#services">Services</a></li>
                 <li><a href="#" className="login" onClick={showLoginModal}>Login</a></li>
-                <li><a href="#" className="signup" onClick={showSignupModal}>Signup</a></li>
+                <li><a href="#" className="login" onClick={showSignupModal}>Signup</a></li>
             </ul>
+            <div className='nav-dropdown' style={{display: showNav ? 'flex' : 'none'}}>   
+                <a href="#home">Home</a>
+                <a href="#services">Services</a>
+                <a href="#"className="login" onClick={showLoginModal}>Login</a>
+                <a href="#" className="login" onClick={showLoginModal}>Signup</a>
+            </div>
         </header>
     );
 }
