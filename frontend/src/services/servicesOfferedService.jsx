ff8321d4fedfa28f7d0c2e408527e39ef04fe99c
lindsey5 <lindseysamson5@gmail.com>
2024-10-20 22:58:33 +0800

    initial commit

--- frontend/src/services/servicesOfferedService.jsx
+++ frontend/src/services/servicesOfferedService.jsx
@@ -1,7 +1,7 @@
 export const addServiceOffered = async (serviceName, setSuccessModal) => {
     if(confirm('Do you want to add this service?')){
      try{
-         const response = await fetch('http://localhost:3000/api/services-offered',{
+         const response = await fetch('h/api/services-offered',{
              method: 'POST',
              headers: {
                  'Content-Type': 'application/json'
