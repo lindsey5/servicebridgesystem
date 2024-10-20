3c96fdfa6d6986d85f2f588be36a4b4c39c7660c
lindsey5 <lindseysamson5@gmail.com>
2024-10-20 23:16:26 +0800

    initial commit

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ import { Server } from 'socket.io';
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT; 
-const origin = 'https://servicebridgesystem.onrender.com/';
+const origin = 'https://servicebridgesystem.onrender.com';
 const io = new Server({
   cors: { origin }
 });
