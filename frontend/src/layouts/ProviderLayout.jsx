704d07b9101a96b5748aee217992ec83d4b3e7a7
lindsey5 <lindseysamson5@gmail.com>
2024-10-22 12:46:48 +0800

    initial commit

--- frontend/src/layouts/ProviderLayout.jsx
+++ frontend/src/layouts/ProviderLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import ProviderHeader from "../Pages/ProviderPage/ProviderHeader.jsx";
-import { ProviderContextProvider, ProviderContext } from "../Context/ProviderContext.jsx";
+import { ProviderContextProvider } from "../Context/ProviderContext.jsx";
 import ProviderSideBar from "../Pages/ProviderPage/ProviderSideBar.jsx";
 import { RecipientContextProvider } from "../Context/RecipientContext.jsx";
 
