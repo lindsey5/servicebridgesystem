28d0f215c5c94ad1a68e89b55c872d22e7e53b0b
lindsey5 <lindseysamson5@gmail.com>
2024-12-07 12:25:40 +0800

    initial commit

--- frontend/src/modals/EditServicePriceModal.jsx
+++ frontend/src/modals/EditServicePriceModal.jsx
@@ -5,7 +5,6 @@ import './EditServicePriceModal.css'
 const EditPriceModal = ({serviceDetails, setServiceDetails, setEditModal, showEditModal}) => {
     // Function to updates the price of provider service offered
     async function updatePrice({service_id, price, description}) {
-        console.log(description)
         // Validate the price
         if (!Number.isInteger(Number(price))) {
             alert("The price should not have decimals");
@@ -13,7 +12,7 @@ const EditPriceModal = ({serviceDetails, setServiceDetails, setEditModal, showEd
             editServiceOfferedPrice(service_id, price, description);
         }
     }
-
+    
     return(
         <div className="edit-price-modal" style={{display: showEditModal ? "flex" : "none" }}>
             <h1>Edit Service</h1>
@@ -21,7 +20,7 @@ const EditPriceModal = ({serviceDetails, setServiceDetails, setEditModal, showEd
             <input className="price" type="number" value={serviceDetails?.price} 
             onChange={(e) => setServiceDetails(detail => ({...detail, price: e.target.value}))}/>
             <p>Description about the service</p>
-            <textarea value={serviceDetails?.description} onChange={(e) => setServiceDetails(detail => ({...detail, description: e.target.value}))}>
+            <textarea value={serviceDetails.description} onChange={(e) => setServiceDetails(detail => ({...detail, description: e.target.value}))}>
 
             </textarea>
             <div className='edit-buttons'>
