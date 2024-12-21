31032cee92086500518970bf008e69fd6a220c3b
lindsey5 <lindseysamson5@gmail.com>
2024-12-21 23:55:14 +0800

    initial commit

--- frontend/src/Pages/Components/Services/Services.jsx
+++ frontend/src/Pages/Components/Services/Services.jsx
@@ -11,12 +11,30 @@ const Services = ({showLoginModal}) =>{
     const next = useRef();
     const back = useRef();
     const { data, loading } = useFetch('/api/category');
+    const [documentWidth, setDocumentWidth] = useState(document.body.scrollWidth);
 
     useEffect(()=>{
         if(data){
             setCategories(data);
         }
     }, [data]);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setDocumentWidth(document.body.scrollWidth);
+        };
+
+        // Set initial width
+        handleResize();
+
+        // Add event listener to handle resize
+        window.addEventListener('resize', handleResize);
+
+        // Cleanup the event listener on unmount
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     const ServicesByCategory = ({category}) => {
         const {data: services} = useFetch(`/api/services/${category}`);
@@ -73,13 +91,21 @@ const Services = ({showLoginModal}) =>{
             {!loading && 
             <>
                 <div className='categories-container'>
-                {categories.length > 0 && categories.slice(start, end).map((category, i) => (
+                {categories.length > 0 && documentWidth < 1005 ? categories.map((category, i) => (
+                    <CategoryDiv key={i} category={category} />
+                ))
+                
+                : categories.slice(start, end).map((category, i) => (
                     <CategoryDiv key={i} category={category} />
                 ))
                 }
                 </div>
+                {documentWidth > 1005 && 
+                <>
                 <button className="prev" style={{display: start === 0 ? 'none' : 'block'}} onClick={backPage} ref={back}>&#10094;</button>
                 <button className="next" style={{display: end === categories.length ? 'none' : 'block'}} onClick={nextPage} ref={next}>&#10095;</button>
+                </>
+                }
             </>
             }
         </div>
